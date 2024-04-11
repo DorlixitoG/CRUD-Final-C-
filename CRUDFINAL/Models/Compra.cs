301b464a7ac1b99580091e7227f5e741fb282441
DorlixitoG <vbkevin.iesanpablo@gmail.com>
2024-04-11 02:43:21 -0500

    Agregar archivos de proyecto.

---
+++ CRUDFINAL/Models/Compra.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUDFINAL.Models;
+
+public partial class Compra
+{
+    public int Id { get; set; }
+
+    public int? IdProveedor { get; set; }
+
+    public DateOnly? Fecha { get; set; }
+
+    public double? Total { get; set; }
+
+    public double? Subtotal { get; set; }
+
+    public double? Descuento { get; set; }
+
+    public double? Iva { get; set; }
+}
---
+++ CRUDFINAL/Models/SoftShirtContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDFINAL.Models;
+
+public partial class SoftShirtContext : DbContext
+{
+    public SoftShirtContext()
+    {
+    }
+
+    public SoftShirtContext(DbContextOptions<SoftShirtContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Compra> Compras { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("server=DESKTOP-QEBBRAK\\SQLEXPRESS;\nTrustServerCertificate=Yes;\n database=Soft_Shirt; integrated security=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Compra>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Compras__3214EC07C4EE42A5");
+
+            entity.Property(e => e.IdProveedor).HasColumnName("Id_proveedor");
+            entity.Property(e => e.Iva).HasColumnName("IVA");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
